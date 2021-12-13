3532a0840dae69f5445acea4ce1ff0fdc9de2a42
zooofzane <yu_an12@163.com>
2021-12-13 00:22:59 -0500

    prototype with 15000 data points

--- blurry_data_visual/src/script.js
+++ blurry_data_visual/src/script.js
@@ -1,32 +1,31 @@
 import './style.css'
 import * as THREE from 'three'
-import {
-    OrbitControls
-} from 'three/examples/jsm/controls/OrbitControls.js'
-import {
-    GUI
-} from 'lil-gui';
-// // import galaxyVertexShader from './shaders/galaxy/vertex.glsl'
-// // import galaxyFragmentShader from './shaders/galaxy/fragment.glsl'
-
-import {
-    CinematicCamera
-} from 'three/examples/jsm/cameras/CinematicCamera.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GUI } from 'lil-gui';
+import VertexShader from './shaders/vertex.glsl'
+import FragmentShader from './shaders/fragment.glsl'
+import { CinematicCamera } from 'three/examples/jsm/cameras/CinematicCamera.js';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 
-const pointer = new THREE.Vector2();
 
+/* -------------------------------------------------------------------------- */
+/*                                  load data                                 */
+/* -------------------------------------------------------------------------- */
 const data = require('./testdata.json');
-console.log(data);
+// console.log(data);
 
+
+/* -------------------------------------------------------------------------- */
+/*                              canvas and scene                              */
+/* -------------------------------------------------------------------------- */
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
-scene.background = new THREE.Color(0xffffff);
+scene.background = new THREE.Color(0xcfcfcf);
 // scene.fog = new THREE.Fog(0xcc0000, 1, 1000);
 
-/* -------------------------------------------------------------------------- */
-/*                                  raycaster                                 */
-/* -------------------------------------------------------------------------- */
+
+/* -------------------------------- raycaster ------------------------------- */
+const pointer = new THREE.Vector2();
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
@@ -37,12 +36,10 @@ function onMouseMove(event) {
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
-/* -------------------------------------------------------------------------- */
-/*                                   guides                                   */
-/* -------------------------------------------------------------------------- */
-const gridHelper = new THREE.GridHelper(10, 10);
+/* --------------------------------- guides --------------------------------- */
+// const gridHelper = new THREE.GridHelper(10, 10);
 // scene.add(gridHelper);
-const axesHelper = new THREE.AxesHelper(5);
+// const axesHelper = new THREE.AxesHelper(5);
 // scene.add(axesHelper);
 
 /* -------------------------------------------------------------------------- */
@@ -53,36 +50,30 @@ function init() {
     let geometry
     let material
 
-    /**
-     * Material
-     */
-    // material = new THREE.ShaderMaterial({
-    //     depthWrite: false,
-    //     blending: THREE.AdditiveBlending,
-    //     vertexColors: true,
-    //     uniforms:
-    //     {
-    //         uTime: { value: 0 },
-    //         uSize: { value: 30 * renderer.getPixelRatio() }
-    //     },    
-    //     vertexShader: galaxyVertexShader,
-    //     fragmentShader: galaxyFragmentShader
-    // })
+    /* -------------------------------- geometry -------------------------------- */
+    geometry = new THREE.SphereGeometry(.005, 32, 32)
+    // geometry = new THREE.PlaneGeometry(1., 1.)
 
+    /* -------------------------------- material -------------------------------- */
+    material = new THREE.ShaderMaterial({
+        depthWrite: false,
+        // blending: THREE.AdditiveBlending,
+        vertexColors: true,
+        uniforms: {
+            // uTime: {
+            //     value: 0
+            // },
+            // uSize: { value: 30 * renderer.getPixelRatio() }
+        },
+        vertexShader: VertexShader,
+        fragmentShader: FragmentShader,
+    })
 
-    /* -------------------------------------------------------------------------- */
-    /*                   map the time yy/mm/dd to absolute value                  */
-    /* -------------------------------------------------------------------------- */
-
+    /* ----------------- map the time yy/mm/dd to absolute value ---------------- */
     let startYear = 1989;
     let endYear = 2020;
     let lowValue_time = startYear * 365;
     let highValue_time = (endYear + 1) * 365;
-
-
-    geometry = new THREE.SphereGeometry(.01, 32, 32)
-    material = new THREE.MeshStandardMaterial()
-
 
 
     for (let i = 0; i < 15950; i++) {
@@ -93,14 +84,11 @@ function init() {
         const object = new THREE.Mesh(geometry, material);
 
         let y_position = map_range(timeAbsValue, lowValue_time, highValue_time, 0, 5);
-        // let y_position = 0;
         let x_position = map_range(latitude, -180, 180, 0, 12) - 6;
-        // let z_position = Math.random() * 8 - 4;
         let z_position = map_range(lontitude, -180, 180, 0, 12) - 6;
         object.position.x = x_position;
         object.position.y = y_position;
         object.position.z = z_position;
-
         /* -------------------------------  details ------------------------------- */
         object.userData.conflict_name = data[i].conflict_name;
         object.userData.country = data[i].country;
@@ -122,33 +110,22 @@ function init() {
 init();
 
 
-/**
- * Sizes
- */
+/* ------------------------------ window resize ----------------------------- */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
-
 window.addEventListener('resize', () => {
-    // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
-
-    // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
-
-    // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 
-/**
- * Camera
- */
-// Base camera
+/* --------------------------------- camera --------------------------------- */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
 camera.position.x = 4
 camera.position.y = 4
@@ -156,25 +133,26 @@ camera.position.z = 4
 // scene.add(new THREE.AmbientLight(0xffffff, 0.3));
 scene.add(camera)
 
+/* ---------------------------------- Stats --------------------------------- */
+let stats = new Stats();
+document.body.appendChild(stats.dom);
 
-
-// Controls
+/* -------------------------------- Controls -------------------------------- */
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**
- * Renderer
- */
+/* -------------------------------- Renderer -------------------------------- */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-console.log(scene.children);
-/**
- * Animate
- */
+
+/* -------------------------------------------------------------------------- */
+/*                                   Animate                                  */
+/* -------------------------------------------------------------------------- */
+
 const clock = new THREE.Clock()
 
 const tick = () => {
@@ -190,7 +168,7 @@ const tick = () => {
 
         // intersects[i].object.material.color.set(0xff0000);
         // console.log(intersects[i].object.userData);
-        console.log(intersects[i].object);
+        // console.log(intersects[i].object);
 
         document.getElementById("conflict_name").innerHTML = "conflict_name:    " + "  " + intersects[i].object.userData.conflict_name;
         document.getElementById("country").innerHTML = "country:    " + "  " + intersects[i].object.userData.country;
@@ -206,15 +184,13 @@ const tick = () => {
         document.getElementById("date_end").innerHTML = "date_end:    " + "  " + intersects[i].object.userData.date_end;
     }
 
-
     // Update material
     // material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
 
-
-
+    stats.update();
 
     // Render
     renderer.render(scene, camera)
@@ -226,13 +202,13 @@ const tick = () => {
 
     window.addEventListener('mousemove', onMouseMove, false);
 }
-
 tick()
 
 
-/* -------------------------------------------------------------------------- */
-/*                                     map                                    */
-/* -------------------------------------------------------------------------- */
+
+
+
+
 function map_range(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
---
+++ blurry_data_visual/src/shaders/fragment.glsl
@@ -0,0 +1,9 @@
+precision mediump float;
+
+// varying vec3 vColor;
+
+void main()
+{
+    vec3 color = vec3(1.0, 0.2863, 0.2863);
+    gl_FragColor = vec4(color, 1.0);
+}
